b87f9122bdaca65c0d0a71b4f74cbcf4d4f5d0c5
John Thompson <johnny918819@gmail.com>
2017-01-23 12:03:07 -0800

    Homework and bonus answer

---
+++ src/homework(1-18-17)/wwwroot/app.js
@@ -0,0 +1,25 @@
+var articles = [
+    "Two teenage girls hospitalised after Whatsapp message unleashed 'Satanic possession'!",
+    "ALIENS took my eggs to create ET-human hybrids, claims ‘abducted woman’",
+    "Are YOU prepared? Zombie outbreak could wipe out humanity in 100 DAYS, experts reveal"
+];
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+    alert(articles[Math.floor(Math.random() * 3)]);
+});
+var things = [
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    true
+];
+for (i = ""; i < things.length; i++) {
+    console.log("" + i);
+}
